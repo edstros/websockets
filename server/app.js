76e94955c4823b78a133dd6c05d0a0b0ddce1bea
Edwin Acevedo <edwin.a.acevedo@gmail.com>
2015-08-27 14:33:33 -0500

    adds socket.io dependencies

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
-'use strict'
+'use strict';
+
 
 var express = require('express');
+var sockcetio = require('socket.io');
+
 var app = express();
 app.use(express.static('client'));
-app.listen(3000, function () {
+var server = app.listen(3000, function () {
   console.log('Server listening on port 3000');
 });
+
+socketio(server);
+
